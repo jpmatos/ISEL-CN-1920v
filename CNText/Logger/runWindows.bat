d97cdbaef1bda9bae61ad30280e3e776aa15f82f
heliofitas@gmail.com <Ysxrafqb1>
2020-06-15 23:46:15 +0100

    Logger app

---
+++ CNText/Logger/runWindows.bat
@@ -0,0 +1,3 @@
+@echo off
+set GOOGLE_APPLICATION_CREDENTIALS=C:\Users\helio.fitas\Dropbox\Helio\ISEL\2019-2020 SV\CN\LABs\Lab6\myKey\g01-li61n-39ec5d38397d.json
+java -cp target/Logger-1.0-SNAPSHOT-jar-with-dependencies.jar Logger
--- CNText/Logger/src/main/java/ReadSubscription.java
+++ CNText/Logger/src/main/java/ReadSubscription.java
@@ -3,8 +3,6 @@ import com.google.cloud.pubsub.v1.Subscriber;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 
 public class ReadSubscription {
-//    private static final String FREE_OCR = "free-ocr"; //TODO delete
-//    private static final String PREMIUM_OCR = "premium-ocr";
     private final Subscriber subscriber;
 
     public ReadSubscription(String subscription, MessageReceiver handler) {
