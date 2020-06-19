fc49ab084c004b81fccf98b5095f68871d29f052
heliofitas@gmail.com <Ysxrafqb1>
2020-06-20 00:58:28 +0100

    Start/Stop VM;

--- CNText/Service/src/main/java/serviceapp/compute/VMManagement.java
+++ CNText/Service/src/main/java/serviceapp/compute/VMManagement.java
@@ -10,15 +10,16 @@ import com.google.api.services.compute.ComputeScopes;
 import com.google.api.services.compute.model.Operation;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
-import serviceapp.Operations;
-import utils.Output;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static serviceapp.Operations.*;
+import static serviceapp.Operations.PROJECT_ID;
+import static utils.Output.OutputType.ERROR;
+import static utils.Output.console;
+import static utils.Output.log;
 
 public class VMManagement {
     private static final String FREE_OCR_VM = "ocr-free-final-project";
@@ -29,6 +30,14 @@ public class VMManagement {
 
     private static final String ZONE = "europe-north1-a";
 
+    private final Object lock = new Object();
+    private final Compute compute;
+    private final Compute.Instances instances;
+
+    public VMManagement() {
+        this.compute = getComputeService();
+        this.instances = compute.instances();
+    }
 
     public static void updateVMInstances(int freeUsers, int premiumUsers) {
         if (freeUsers > 0) {
@@ -40,39 +49,121 @@ public class VMManagement {
         //TODO update Premium VM's
     }
 
-    public static void main(String... args) throws IOException, GeneralSecurityException {
-        Output.console("teste");
-//        startVM();
-        stopVM();
+    public static void main(String... args) throws IOException {
+        console("teste");
+
+        VMManagement management = new VMManagement();
+
+        new Thread(() -> management.startStop(true)).start();
+        new Thread(() -> management.startStop(false)).start();
+        new Thread(() -> management.startStop(true)).start();
+
+
+        while (true) {
+            try {
+                Thread.sleep(10_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    private static void startVM() throws IOException, GeneralSecurityException {
-        Compute computeService = getComputeService();
-        Operation operation = computeService.instances()
-                .start(PROJECT_ID, ZONE, FREE_OCR_VM)
+    private void startStop(boolean start) {
+        log("Start VM: " + start);
+        synchronized (lock) {
+            try {
+                if (start) {
+                    startVM(FREE_OCR_VM);
+                } else {
+                    stopVM(FREE_OCR_VM);
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * https://cloud.google.com/compute/docs/reference/rest/v1/instances/start
+     */
+    private void stopVM(String instance) throws IOException {
+        console("Stoping VM...");
+        Operation operation = instances.stop(PROJECT_ID, ZONE, instance)
                 .execute();
+//            triggerConclusion(operation, "STOP_VM");
+        waitOperation(operation);
+        console("VM status: " + getStatus(instance));
     }
 
-    private static void stopVM() throws IOException, GeneralSecurityException {
-        Compute computeService = getComputeService();
-        Operation operation = computeService.instances()
-                .stop(PROJECT_ID, ZONE, FREE_OCR_VM)
+    private void startVM(String instance) throws IOException {
+        console("Starting VM...");
+        Operation operation = instances.start(PROJECT_ID, ZONE, instance)
                 .execute();
+//            triggerConclusion(operation, "START_VM");
+        waitOperation(operation);
+        console("VM status: " + getStatus(instance));
     }
 
-    private static Compute getComputeService() throws IOException, GeneralSecurityException {
-        GoogleCredentials credential = GoogleCredentials.getApplicationDefault();
-        List<String> scopes = new ArrayList<>();
-        scopes.add(ComputeScopes.COMPUTE);
-        credential = credential.createScoped(scopes);
-        HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
-        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        HttpRequestInitializer requestInit = new HttpCredentialsAdapter(credential);
+    private String getStatus(String instance) throws IOException {
+        return instances.get(PROJECT_ID, ZONE, instance)
+                .execute()
+                .getStatus();
+    }
 
-        return new Compute
-                .Builder(transport, jsonFactory, requestInit)
-                .setApplicationName("gRPC Service G01")
-                .build();
+    private void triggerConclusion(final Operation op, String opType) {
+        new Thread(() -> {
+            try {
+                Operation operation = op;
+                while (!operation.getStatus().equals("DONE")) {
+                    console(opType + " " + operation.getStatus());
+                    Thread.sleep(1_000);
+                    operation = compute.zoneOperations()
+                            .get(PROJECT_ID, ZONE, operation.getName())
+                            .execute();
+                }
+                console("<<< " + opType + " " + operation.getStatus());
+            } catch (InterruptedException | IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }).start();
+    }
+
+    private void waitOperation(Operation op) {
+        try {
+            while (!op.getStatus().equals("DONE")) {
+                Thread.sleep(1_000);
+                Compute.ZoneOperations.Get get = compute.zoneOperations().get(PROJECT_ID, ZONE, op.getName());
+                op = get.execute();
+                console(op.getStatus());
+            }
+        } catch (InterruptedException | IOException ex) {
+            log(ERROR, ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    private Compute getComputeService() {
+        try {
+            GoogleCredentials credential = GoogleCredentials.getApplicationDefault();
+            List<String> scopes = new ArrayList<>();
+            scopes.add(ComputeScopes.COMPUTE);
+            credential = credential.createScoped(scopes);
+            HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+            HttpRequestInitializer requestInit = new HttpCredentialsAdapter(credential);
+
+            return new Compute
+                    .Builder(transport, jsonFactory, requestInit)
+                    .setApplicationName("gRPC Service G01")
+                    .build();
+
+        } catch (IOException | GeneralSecurityException ex) {
+            ex.printStackTrace();
+            log(ERROR, ex.getMessage());
+            return null;
+        }
     }
 
 
