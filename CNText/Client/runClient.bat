8a08a58ae5b22cd4017244d5547cb8aa648bccbc
heliofitas@gmail.com <Ysxrafqb1>
2020-06-21 13:05:22 +0100

    UTF-8 fix

--- CNText/Client/runClient.bat
+++ CNText/Client/runClient.bat
@@ -1,1 +1,3 @@
-java -cp ../lib/rpcContract-1.0.jar;Client-1.0-jar-with-dependencies.jar clientapp.Client
+@echo off
+cls
+java -cp ../lib/rpcContract-1.0.jar;target/Client-1.0-jar-with-dependencies.jar clientapp.Client 35.228.117.120 8000
